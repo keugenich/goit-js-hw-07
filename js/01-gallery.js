102a9a957581575006ecd7466f3e40d9cfd4f54c
keugenich <mr.bumbastic.ek@gmail.com>
2023-08-27 20:32:43 +0100

    JS HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,24 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryList = document.querySelector('.gallery');
+let instance = null;
 
 galleryList.addEventListener('click', (event) => {
     event.preventDefault();
 
     if (event.target.nodeName === 'IMG') {
         const imageSrc = event.target.dataset.source;
-        const instance = basicLightbox.create(`
-      <img src="${imageSrc}" width="800" height="600">
-    `);
+        instance = basicLightbox.create(`
+          <img src="${imageSrc}" width="800" height="600">
+        `);
         instance.show();
+
+        document.addEventListener('keydown', onKeyDown);
     }
 });
 
-document.addEventListener('keydown', (event) => {
+function onKeyDown(event) {
     if (event.key === 'Escape') {
         instance.close();
+        document.removeEventListener('keydown', onKeyDown);
     }
-});
+}
 
 
 console.log(galleryItems);
